b5b4a61db8ed155f9f0198a567c0644a67d0c3c6
unknown <stharj219@gmail.com>
2022-07-20 00:02:30 +0545

    Verification changes Users can verify with args 'visitors' as guest if they don't have genshin Will remove the visitors role if user verifies with UID again

--- commands/verify.js
+++ commands/verify.js
@@ -1,31 +1,56 @@
-const { Discord, MessageEmbed } = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+
+const travelersID = "998631828896370719";
+const guestID = "998974130349162527";
+
 module.exports.run = async (client, msg, args) => {
+  // ID of roles user has
   const roles = msg.member._roles;
-  const something = roles.map((role) => {
+
+  // Roles user has
+  const userRolesByName = roles.map((role) => {
     return msg.guild.roles.cache.find((r) => r.id === role).name;
   });
-  if (something.some((role) => role.length === 9)) {
-    const exampleEmbed = await new MessageEmbed().setColor("#ff0000").setTitle("Failed \\❌ ").setDescription(`You're already verified. `).setTimestamp();
 
+  // Chekcs if user has a role with name of 9 lenght which is bascially UUID
+  // If he is already verified
+  if (userRolesByName.some((role) => role.length === 9)) {
+    const exampleEmbed = await new MessageEmbed().setColor("#ff0000").setTitle("Failed \\❌ ").setDescription(`You're already verified. `).setTimestamp();
     return await msg.channel.send(exampleEmbed);
   }
 
+  // Chekcs if the command reveiced args
   if (args.length <= 0 || args.length >= 2) {
     // inside a command, event listener, etc.
     const exampleEmbed = await new MessageEmbed().setColor("#ff0000").setTitle("Failed \\❌ ").setDescription(`UID not specified, Please try again!`).setFooter("useage: ?verify <UID> ").setTimestamp();
-
-    return await msg.channel.send(exampleEmbed);
-  } else if (isNaN(args[0]) || args[0].length !== 9) {
-    const exampleEmbed = await new MessageEmbed().setColor("#ff0000").setTitle("Failed \\❌ ").setDescription(`Failed to verify UID: ${args[0]}. Please check your UID and try again !`).setFooter("useage: ?verify <UID> ").setTimestamp();
-
-    return await msg.channel.send(exampleEmbed);
-  } else if (msg.member.roles.cache.some((role) => role.name === args[0])) {
-    const exampleEmbed = await new MessageEmbed().setColor("#ff0000").setTitle("Failed \\❌ ").setDescription(`You're already verified. `).setTimestamp();
-
     return await msg.channel.send(exampleEmbed);
   }
 
+  // Checks if the user already has visitor role if he tries to verify with visitor again
+  if (userRolesByName.some((role) => role.length === 9) && args[0] === "visitor") {
+    const exampleEmbed = await new MessageEmbed().setColor("#ff0000").setTitle("Failed \\❌ ").setDescription(`You're already verified as visitor `).setTimestamp();
+    return await msg.channel.send(exampleEmbed);
+  }
+
+  // Chekcs if the argument is "visitor", which assigns the user with guest role instead of UUID and Travelers role
+  else if (args[0] === "visitor") {
+    msg.member.roles.add("998974130349162527");
+    const exampleEmbed = await new MessageEmbed().setColor("#2E2EAC").setTitle("Verified \\✅").setDescription(`You have been verified with UID: ${args[0]}.\n `).setTimestamp();
+    return await msg.channel.send(exampleEmbed);
+  }
+  // checks if the UID user provided is valid uid
+  else if (isNaN(args[0]) || args[0].length !== 9) {
+    const exampleEmbed = await new MessageEmbed().setColor("#ff0000").setTitle("Failed \\❌ ").setDescription(`Failed to verify UID: ${args[0]}. Please check your UID and try again !`).setFooter("useage: ?verify <UID> ").setTimestamp();
+    return await msg.channel.send(exampleEmbed);
+  }
+  // If everything works out, now we will create a role with the UID user provided and assign it to him with travelrs role
   const person = msg.member;
+
+  // Removes the guest role if the user already has it
+  try {
+    const guestRole = msg.guild.roles.cache.get(guestID);
+    msg.member.roles.remove(guestRole);
+  } catch (err) {}
   msg.guild.roles
     .create({
       data: {
@@ -35,7 +60,7 @@ module.exports.run = async (client, msg, args) => {
       reason: "we needed a role for Super Cool People",
     })
     .then((role) => {
-      person.roles.add("998631828896370719");
+      person.roles.add(travelersID);
       person.roles.add(role.id);
     })
     .catch(console.error);
--- index.js
+++ index.js
@@ -1,30 +1,37 @@
-const fs = require("fs");
 require("dotenv").config();
 const Discord = require("discord.js");
+const fs = require("fs");
 const client = new Discord.Client();
+
 const prefix = "?";
 
 client.commands = new Discord.Collection();
 const commands = fs.readdirSync("./commands").filter((file) => file.endsWith(".js"));
+
+// Loading in commands
 for (file of commands) {
   const commandName = file.split(".")[0];
   const command = require(`./commands/${commandName}`);
   client.commands.set(commandName, command);
 }
 
+// On ready
 client.on("ready", () => {
   client.user.setActivity("Genshin Hub", { type: "WATCHING" });
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+// On message receive
 client.on("message", async (msg) => {
+  // Calls in command function if message starts ith prefix and if commands exists
   if (msg.content.startsWith(prefix)) {
     const args = msg.content.slice(prefix.length).trim().split(/ +/g);
     const commandName = args.shift();
     const command = client.commands.get(commandName);
     if (!command) return;
-    command.run(client, msg, args);
+    return command.run(client, msg, args);
   }
 });
 
+// Run the client with token form .env file.
 client.login(process.env.TOKEN);
